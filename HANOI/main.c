c37cb479349dd8463045a06945a6a93b7688d4ad
KKQC <aleksander@demehlem.com>
2022-10-29 23:33:44 +0200

    Initial Commit added comment

---
+++ HANOI/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  HANOI
+//
+//  Created by Aleksander von Mehlem on 29/10/2022.
+//
+//  I wrote this program to help me with my IA in mathematics.
+
+/* C program for Tower of Hanoi*/
+#include <stdio.h>
+void towerfun(int, char, char, char);
+int main()
+{
+    int n;// defined to store the number disc
+    printf("Enter the number of disks : ");
+    scanf("%d", &n);
+    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
+    towerfun(n, 'A', 'C', 'B'); // A, B, C are tower
+    return 0;
+}
+
+void towerfun(int n, char fr, char tr, char ar)
+{
+    if (n == 1)
+    {
+        printf("\n Move disk 1 from rod %c to rod %c", fr, tr);
+        return;
+    }
+    towerfun(n - 1, fr, ar, tr);
+    printf("\n Move disk %d from rod %c to rod %c", n, fr, tr);
+    towerfun(n - 1, ar, tr, fr);
+}
